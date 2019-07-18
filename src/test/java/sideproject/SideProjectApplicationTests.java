7825ed41b4b1d49a15af7515c72836eaddf13aad
Sshipley12 <steph.shipley@yahoo.com>
2019-07-18 13:35:08 -0400

    inital commit

---
+++ src/test/java/sideproject/SideProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package sideproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SideProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
